6472792117f146cf9919f9762ec30f4ef90612f5
Shaheb746 <146575639+Shaheb746@users.noreply.github.com>
2024-02-03 16:05:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let height = parseInt(document.querySelector('#height').value);
+    let weight = parseInt(document.querySelector('#weight').value);
+
+    const guid = document.querySelector('#weight-para');
+    let result = document.querySelector('#result');
+
+    if (height === '' || height < 0 || isNaN(height)) {
+        result.innerHTML = 'Please enter a valid height!';
+    } else if (weight === '' || weight < 0 || isNaN(weight)) {
+        result.innerHTML = 'Please enter a valid weight!';
+    } else {
+        const BMI = (weight / ((height * height) / 10000)).toFixed(2);
+        result.innerHTML = `<span>${BMI}</span>`;
+    }
+
+    if (height === '' || height < 0 || isNaN(height) || weight === '' || weight < 0 || isNaN(weight)) {
+        guid.innerHTML = '';
+    } else {
+        if (weight < 18.6) {
+            guid.innerHTML = `Your weight is Underweight`;
+        } else if (weight >= 18.6 && weight <= 24.9) {
+            guid.innerHTML = `Your weight is Normal`;
+        } else {
+            guid.innerHTML = `Your weight is Overweight`;
+        }
+    }
+});
